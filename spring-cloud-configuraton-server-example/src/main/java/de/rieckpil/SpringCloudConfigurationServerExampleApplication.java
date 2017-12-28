a8f39bba2a324c28d8447ac0487170ff68dadf1d
Philip Riecks <philip@riecks.io>
2017-12-28 15:36:41 +0100

    add simple config server

--- spring-cloud-configuraton-server-example/src/main/java/de/rieckpil/SpringCloudConfigurationServerExampleApplication.java
+++ spring-cloud-configuraton-server-example/src/main/java/de/rieckpil/SpringCloudConfigurationServerExampleApplication.java
@@ -2,11 +2,13 @@ package de.rieckpil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudConfigurationServerExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudConfigurationServerExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigurationServerExampleApplication.class, args);
+    }
 }
