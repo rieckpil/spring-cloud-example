566b2e6e8981377a0eb453ef766ee31ecdf61a3f
Philip Riecks <philip@riecks.io>
2017-12-29 09:55:13 +0100

    add spring cloud config bus to project

--- spring-cloud-microservice-example/src/main/java/de/rieckpil/controllers/CloudController.java
+++ spring-cloud-microservice-example/src/main/java/de/rieckpil/controllers/CloudController.java
@@ -3,11 +3,13 @@ package de.rieckpil.controllers;
 import de.rieckpil.services.CloudService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RefreshScope
 public class CloudController {
 
     @Value("${lucky-word}")
