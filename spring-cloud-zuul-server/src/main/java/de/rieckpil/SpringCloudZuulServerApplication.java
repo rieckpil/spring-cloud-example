54c40059af3ff587f7c5800f0cec4b845259b016
Philip Riecks <philip@riecks.io>
2017-12-30 06:22:24 +0100

    implement simple zuul routing

--- spring-cloud-zuul-server/src/main/java/de/rieckpil/SpringCloudZuulServerApplication.java
+++ spring-cloud-zuul-server/src/main/java/de/rieckpil/SpringCloudZuulServerApplication.java
@@ -2,11 +2,13 @@ package de.rieckpil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class SpringCloudZuulServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudZuulServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudZuulServerApplication.class, args);
+    }
 }
