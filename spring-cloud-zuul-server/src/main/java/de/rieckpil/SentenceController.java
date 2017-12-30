aa09f14134c2cc24e2a6f7e5078df9ed676e1c14
Philip Riecks <philip@riecks.io>
2017-12-30 06:11:43 +0100

    add simple zuul server to project

---
+++ spring-cloud-zuul-server/src/main/java/de/rieckpil/SentenceController.java
@@ -0,0 +1,13 @@
+package de.rieckpil;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SentenceController {
+
+    @GetMapping("/")
+    public String sentence() {
+        return "sentence";
+    }
+}
